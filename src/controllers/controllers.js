8ece0162f01ea26440fe2c7728afdd1de8d0cfd6
Melitchi <melissa.fontaine4@gmail.com>
2016-11-27 23:49:48 +0100

    Added services that provides data
    Added angular material list
    Provider supply list with data

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -3,24 +3,24 @@
 /* Controllers */
 
 app
-
 /////////////////////// list controller  ///////////////////////////////////////////////////
-.controller('listCtrl', ['$scope', '$ngRoute', function ($scope, $ngRoute) {
+.controller('listCtrl',  function ($scope, $routeParams, devisProvider) {
+  console.log("heyo!");
   var param=$routeParams.param; // on récupère la valeur passée dans l'url
   $scope.displayDevis=false;
-  $scope.displayCatalogue=false;
-  $scope.displayFournisseur=false;
-  $scope.sortReverse = false;  // sens du tri par defaut
-  $scope.searchText = ''; // vide le champ de recherche
+  //$scope.displayCatalogue=false;
+  //$scope.displayFournisseur=false;
+  //$scope.sortReverse = false;  // sens du tri par defaut
+  //$scope.searchText = ''; // vide le champ de recherche
   // Test pour récupérer les données correspondant au paramètre envoyé
+  $scope.pageTitle ="titre switch";
 
   switch (param) {
     case "devis":
     $scope.pageTitle ="Liste des devis";
     $scope.sortType = 'num_devis'; // tri sur le num_devis par defaut
-    /*$http.get("liste_devis.json").then(function (response) {
-      $scope.datas = response.data;
-    });*/
+    $scope.datas = devisProvider.getDevis();
+    $scope.labels=devisProvider.getDevisLabels();
     $scope.displayDevis=true;
     /*$scope.getAvancementColor = function (input) {
       if (input == "refusé") {
@@ -31,16 +31,16 @@ app
       }
     }*/
     break;
-    /*case "catalogue":
-    $scope.pageTitle ="Catalogue";
+    case "catalogue":
+  /*  $scope.pageTitle ="Catalogue";
     $http.get("catalogue.json").then(function (response) {
       $scope.catalogue = response.data;
     });
     $scope.displayCatalogue=true;
-
+*/
     break;
     case "fournisseurs":
-    $scope.pageTitle ="Liste des fournisseurs";
+    /*$scope.pageTitle ="Liste des fournisseurs";
     $http.get("liste_fournisseurs.json").then(function (response) {
       $scope.fournisseurs = response.data;
     });
@@ -48,4 +48,4 @@ app
     break;*/
     default:
   }
-}]);
+})
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,3 +1,4 @@
+'use strict';
 app
 .config(function($mdThemingProvider) {
   var customBlueMap = 		$mdThemingProvider.extendPalette('orange', {
