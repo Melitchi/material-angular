3f454369cc3084ab718b623552056958275b815c
Melitchi <melissa.fontaine4@gmail.com>
2016-12-05 10:41:38 +0100

    initialize devis filters
    added icons

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -8,50 +8,42 @@ app
   console.log("heyo!");
   var param=$routeParams.param; // on récupère la valeur passée dans l'url
   $scope.displayDevis=false;
-  //$scope.displayCatalogue=false;
-  //$scope.displayFournisseur=false;
-  $scope.sortReverse = true;  // sens du tri par defaut
+  $scope.displayCatalogue=false;
+  $scope.displayFournisseur=false;
+  $scope.sortReverse = false;  // sens du tri par defaut
   $scope.searchText = ''; // vide le champ de recherche
   // Test pour récupérer les données correspondant au paramètre envoyé
-  $scope.pageTitle ="titre switch";
   $scope.getSelectedText = function() {
-          if ($scope.sortType !== undefined) {
-            console.log("You have selected: Item " + $scope.sortType);
-          } else {
-            console.log("Please select an item");
-          }
-        };
+    if ($scope.sortType !== undefined) {
+      console.log("You have selected: Item " + $scope.sortType);
+    } else {
+      console.log("Please select an item");
+    }
+  };
   switch (param) {
     case "devis":
-    $scope.pageTitle ="Liste des devis";
-  //  $scope.sortType = 'num_devis'; // tri sur le num_devis par defaut
-    $scope.datas = devisProvider.getDevis();
-    $scope.labels=devisProvider.getDevisLabels();
-    $scope.displayDevis=true;
-    /*$scope.getAvancementColor = function (input) {
-      if (input == "refusé") {
-        return "bold redFont";
-      }
-      else if (input == "terminé") {
-        return "bold greenFont";
-      }
-    }*/
+      $scope.pageTitle ="Liste des devis";
+      $scope.sortType = 'num_devis'; // tri sur le num_devis par defaut
+      $scope.datas = devisProvider.getDevis();
+      $scope.labels=devisProvider.getDevisLabels();
+      $scope.displayDevis=true;
+      /*$scope.getAvancementColor = function (input) {
+        if (input == "refusé") {
+          return "bold redFont";
+        }
+        else if (input == "terminé") {
+          return "bold greenFont";
+        }
+      }*/
     break;
     case "catalogue":
-  /*  $scope.pageTitle ="Catalogue";
-    $http.get("catalogue.json").then(function (response) {
-      $scope.catalogue = response.data;
-    });
-    $scope.displayCatalogue=true;
-*/
+      $scope.pageTitle ="Catalogue";
+      $scope.displayCatalogue=true;
     break;
     case "fournisseurs":
-    /*$scope.pageTitle ="Liste des fournisseurs";
-    $http.get("liste_fournisseurs.json").then(function (response) {
-      $scope.fournisseurs = response.data;
-    });
-    $scope.displayFournisseur=true;
-    break;*/
+      $scope.pageTitle ="Liste des fournisseurs";
+      $scope.displayFournisseur=true;
+    break;
     default:
   }
 })
